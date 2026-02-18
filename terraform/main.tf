601acdf518d70e0693f4ba71e9d7f56a8c9c55fd
Aarya Jamwal <aaryajamwal90@gmail.com>
2026-02-19 00:14:02 +0530

    add: http, https, jenkins port sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,10 +24,35 @@ resource "aws_security_group" "my-ec2-sg" {
   description = "allow ssh from my ip"
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.allowed_ssh_ip]
+  }
+
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # Restrict in production
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
